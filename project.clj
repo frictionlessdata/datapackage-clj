0848f7985265beb3a853ffa25f643409d990508b
_mthom <matt@mthompson.org>
2017-10-26 09:44:29 +0100

    Initial commit of project structure & travis/coveralls conf

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject datapackage-clj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]]
+  :plugins [[lein-cloverage "1.0.9"]])
---
+++ test/datapackage_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns datapackage-clj.core-test
+  (:require [clojure.test :refer :all]
+            [datapackage-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
